eb90195c1cbcc266d86821ae6afa4c2b4f2ccc64
Dannyfsp <femoscopilala@gmail.com>
2022-11-04 22:30:23 -0700

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,45 +8,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head_tmp1, *head_tmp2, *aux1, *aux2;
-	int flag;
+	listint_t *aux;
 
-	if (list)
+	if (list == NULL || *list == NULL)
+		return;
+
+	aux = (*list)->next;
+
+	while (aux != NULL)
 	{
-		head_tmp1 = *list;
-		head_tmp2 = *list;
-		while (list && head_tmp1->next)
+		while (aux->prev && aux->prev->n > aux->n)
 		{
-			if (head_tmp1->next)
-			{
-				flag = 0;
-				head_tmp2 = head_tmp1;
-				while (head_tmp2 && head_tmp2->n > head_tmp2->next->n)
-				{
-					aux1 = head_tmp2;
-					aux2 = head_tmp2->next;
-					aux1->next = aux2->next;
-					if (aux2->next)
-						aux2->next->prev = aux1;
-					if (aux2)
-					{
-						aux2->prev = aux1->prev;
-						aux2->next = aux1;
-					}
-					if (aux1)
-						aux1->prev = aux2;
-					if (aux2->prev)
-						aux2->prev->next = aux2;
-					head_tmp2 = aux2->prev;
-					if (!aux2->prev)
-						*list = aux2;
-					print_list(*list);
-					flag = 1;
-				}
-			}
-			if (flag == 0)
-				head_tmp1 = head_tmp1->next;
-		}
-	}
+			aux->prev->next = aux->next;
+			if (aux->next)
+				aux->next->prev = aux->prev;
+			aux->next = aux->prev;
+			aux->prev = aux->next->prev;
+			aux->next->prev = aux;
 
+			if (aux->prev == NULL)
+				*list = aux;
+			else
+				aux->prev->next = aux;
+			print_list(*list);
+		}
+		aux = aux->next;
+	}
 }
